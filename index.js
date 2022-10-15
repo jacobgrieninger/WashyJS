c9cc46810f3239ba9892fcab0b28b390d2095a2f
jacobgrieninger <jacob.grieninger@gmail.com>
2022-10-15 16:29:03 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+const axios = require("axios");
+const moment = require("moment");
+const queryString = require("query-string");
+const schedule = require("node-schedule");
+
+async function getForecast() {
+  let results = {
+    willRain: false,
+    rainyDays: [],
+    rainPercent: [],
+  };
+  const now = moment.utc();
+  const startTime = moment.utc(now).add(0, "minutes").toISOString();
+  const endTime = moment.utc(now).add(5, "days").toISOString();
+
+  await axios
+    .get(
+      `https://api.tomorrow.io/v4/timelines?location=34.254691,-77.846289&fields=precipitationProbability&fields=precipitationType&timesteps=1h&units=imperial&apikey=YzYG8RRkV6EzJlZg1XF1fo05Rdb7nDyN&${queryString.stringify(
+        { startTime, endTime }
+      )}`
+    )
+    .then((res) => {
+      res.data.data.timelines[0].intervals.forEach((obj) => {
+        if (obj.values.precipitationProbability >= 20) {
+          if (results.willRain == false) {
+            results.willRain = true;
+          }
+          results.rainyDays.push(obj.startTime);
+          results.rainPercent.push(obj.values.precipitationProbability);
+        }
+      });
+    });
+  return results;
+}
+
+// Create a new client instance
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+  const job = schedule.scheduleJob("0 6 * * *", async function () {
+    (async () => {
+      let result = await getForecast();
+      console.log(result);
+      if (result.willRain == true) {
+        client.channels.cache
+          .find((channel) => channel.name == "washy")
+          .send("Today is not a good day to get a car wash!");
+      } else {
+        client.channels.cache
+          .find((channel) => channel.name == "washy")
+          .send("Today is a good day to get a car wash!");
+      }
+    })();
+  });
+});
+
+// Login to Discord with your client's token
+client.login(token);
